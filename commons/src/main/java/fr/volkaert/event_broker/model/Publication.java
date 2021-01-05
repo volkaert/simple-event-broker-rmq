d936b2e1502f2fc308aa68d51e1c0fddd836bd18
Fabrice VOLKAERT <fabrice_volkaert@yahoo.fr>
2021-01-05 14:57:33 +0100

    authClientIds added in the Publication class (will null values in the catalog for the moment)

--- commons/src/main/java/fr/volkaert/event_broker/model/Publication.java
+++ commons/src/main/java/fr/volkaert/event_broker/model/Publication.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Arrays;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -19,4 +21,9 @@ public class Publication {
     private String name;
     private String eventTypeCode;
     private boolean active;
+
+    // Use either List<String> authClientIdsAsList = Arrays.asList(clientId1, clientId2, clientId3); String authClientIds = String.join(",", authClientIdsAsList);
+    // or String[] authClientIdsAsArray = { clientId1, clientId2, clientId3 }; String authClientIds = String.join(",", authClientIdsAsArray);
+    // In the opposite way, to convert the String to an Array, just do String[] authClientIdsAsArray = authClientIds.split(",");
+    private String authClientIds; // Authent ClientIds (comma separated) that are allowed to publish using this Publication (if null, no check will be performed)
 }
