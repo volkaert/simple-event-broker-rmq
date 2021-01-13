d79ebb5195f54135e12cb589a973e60a318b8568
Fabrice VOLKAERT <fabrice_volkaert@yahoo.fr>
2021-01-13 01:04:07 +0100

    StringHttpMessageConverter added for RestTemplate

--- commons/src/main/java/fr/volkaert/event_broker/catalog_adapter_client/CatalogAdapterClientConfig.java
+++ commons/src/main/java/fr/volkaert/event_broker/catalog_adapter_client/CatalogAdapterClientConfig.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class CatalogAdapterClientConfig {
@@ -21,12 +24,15 @@ public class CatalogAdapterClientConfig {
     @Qualifier("RestTemplateForCatalogAdapter")
     //@LoadBalanced
     public RestTemplate restTemplateForCatalogAdapter(RestTemplateBuilder builder) {
+        RestTemplate restTemplate = null;
         if (! StringUtils.isEmpty(authClientIdForCatalogAdapter) && ! StringUtils.isEmpty(authClientSecretForCatalogAdapter))
-            return builder
+            restTemplate = builder
                     .basicAuthentication(authClientIdForCatalogAdapter, authClientSecretForCatalogAdapter)
                     .build();
         else
-            return builder
+            restTemplate = builder
                     .build();
+        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
     }
 }
