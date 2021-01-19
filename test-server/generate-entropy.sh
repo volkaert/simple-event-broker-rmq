e2acf8771c6d2b621f0f39890d46f213433e00f3
Fabrice VOLKAERT <fabrice_volkaert@yahoo.fr>
2021-01-19 23:18:55 +0100

    generate-entropy.sh script added

---
+++ test-server/generate-entropy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# In a terminal 1, run curl "http://localhost:8090/tests/nominal/pub/run?n=1000000000&pause=500"
+# In a terminal 2, run the generate-entropy.sh script
+
+while true; do
+  curl http://localhost:8090/tests/nominal/sub/reject
+  sleep 20
+  curl http://localhost:8090/tests/nominal/sub/accept
+  sleep 20
+done
